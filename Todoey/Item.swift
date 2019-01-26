31c09fa16e029dc33408faceb2ca0018c2cd0e6b
Eisa <eisa@eisas-mac.local>
2019-01-25 23:15:58 -0800

    Convert To MVC Design Pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Eisa on 1/25/19.
+//  Copyright © 2019 Eisa. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title:String = ""
+    var done: Bool = false
+    
+
+}
